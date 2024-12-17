9bf8c5857728f5549c9db6c0a981503bece17bc2
Sebastian Nickel <nick@nine.ch>
2024-12-17 15:25:25 +0100

    allow to attach labels to the nameserver pod

--- cmd/k8s-operator/nameserver.go
+++ cmd/k8s-operator/nameserver.go
@@ -167,11 +167,12 @@ func nameserverResourceLabels(name, namespace string) map[string]string {
 }
 
 func (a *NameserverReconciler) maybeProvision(ctx context.Context, tsDNSCfg *tsapi.DNSConfig) error {
-	labels := nameserverResourceLabels(tsDNSCfg.Name, a.tsNamespace)
+	resourceLabels := nameserverResourceLabels(tsDNSCfg.Name, a.tsNamespace)
 	dCfg := &deployConfig{
 		ownerRefs: []metav1.OwnerReference{*metav1.NewControllerRef(tsDNSCfg, tsapi.SchemeGroupVersion.WithKind("DNSConfig"))},
 		namespace: a.tsNamespace,
-		labels:    labels,
+		labels:    resourceLabels,
+		podLabels: tsDNSCfg.Spec.PodLabels,
 		imageRepo: defaultNameserverImageRepo,
 		imageTag:  defaultNameserverImageTag,
 		replicas:  1,
@@ -225,6 +226,7 @@ type deployConfig struct {
 	imageRepo   string
 	imageTag    string
 	labels      map[string]string
+	podLabels   map[string]string
 	ownerRefs   []metav1.OwnerReference
 	namespace   string
 	clusterIP   string
@@ -258,6 +260,13 @@ var (
 			d.ObjectMeta.Labels = cfg.labels
 			d.ObjectMeta.OwnerReferences = cfg.ownerRefs
 			d.Spec.Template.Spec.Tolerations = cfg.tolerations
+			if d.Spec.Template.Labels == nil {
+				d.Spec.Template.Labels = make(map[string]string)
+			}
+			for key, value := range cfg.podLabels {
+				d.Spec.Template.Labels[key] = value
+			}
+
 			updateF := func(oldD *appsv1.Deployment) {
 				oldD.Spec = d.Spec
 			}
